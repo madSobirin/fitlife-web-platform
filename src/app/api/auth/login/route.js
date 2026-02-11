e5b7a9f33c609146f6128fb5fc4cb417c44edbe2
madSob <gamingbrn202@gmail.com>
2026-02-12 01:01:34 +0700

    membuat schema di prisma

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request) {
   try {
     const body = await request.json();
